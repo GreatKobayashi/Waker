0f15e1595d8546e33aa7aff79c5eee6d919e7bd6
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2025-10-22 19:08:27 +0900

    最低限の状態で登録

---
+++ src/Waker.Root/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Waker.Root
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Waker.Root" };
+        }
+    }
+}
---
+++ src/Waker.Root/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Waker.Root
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
